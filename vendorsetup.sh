53d220f2529f4e979c443c0642badf5538560f81
speck99 <hector_speed@hotmail.com>
2019-02-02 23:46:09 -0800

    disable patches for nougat

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd frameworks/native
-git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/nicklaus/patches/0002-frameworks-native.patch
-cd ../..
-cd system/netd
-git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/nicklaus/patches/0010-wifi-tethering-fix.patch
-cd ../..
+#cd frameworks/native
+#git reset --hard && git clean -f -d
+#patch -p1 < ../../device/motorola/nicklaus/patches/0002-frameworks-native.patch
+#cd ../..
+#cd system/netd
+#git reset --hard && git clean -f -d
+#patch -p1 < ../../device/motorola/nicklaus/patches/0010-wifi-tethering-fix.patch
+#cd ../..
 add_lunch_combo aosp_nicklaus-user
 add_lunch_combo aosp_nicklaus-userdebug
 add_lunch_combo aosp_nicklaus-eng
